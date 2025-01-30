1036f5e68412d93078893cfd57acd0e23d9b7e6a
Joanna Sharon D <joannasharon.d2023cse@sece.ac.in>
2025-01-30 11:43:58 +0530

    Updated

--- subs/src/pages/Navbar.jsx
+++ subs/src/pages/Navbar.jsx
@@ -14,6 +14,7 @@ const Navbar = () => {
           <Link to="/cart">Cart</Link>
           <Link to="/signup">Signup</Link>
           <Link to="/orders">Orders</Link>
+          <Link to="/signup">Logout</Link>
         </li>
       </ul>
     </nav>
